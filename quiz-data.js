fc30f4ce2f2bc5a8a07ee18eaeca758501bd1f59
Heizz1122 <88568173+Heizz1122@users.noreply.github.com>
2025-10-17 20:52:48 +0800

    Update quiz-data.js

--- quiz-data.js
+++ quiz-data.js
@@ -1,13 +1,7 @@
 const quiz = [
   {
-    question: "Apakah planet terbesar dalam sistem suria?",
+    question: "Apakah planet terbesar?",
     options: ["Bumi", "Marikh", "Jupiter", "Uranus"],
     answer: 2
-  },
-  {
-    question: "Apakah fungsi utama daun pada tumbuhan?",
-    options: ["Menyerap air", "Menyerap cahaya", "Menyokong batang", "Menyimpan makanan"],
-    answer: 1
-  },
-  // Tambah lagi soalan di sini
+  }
 ];
